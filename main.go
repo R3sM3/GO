ca903ba9bcbd3a64891bafa990c24d4a47e580e8
Raúl <rsmescobar@gmail.com>
2022-01-02 18:49:19 -0300

    Primer código

---
+++ main.go
@@ -0,0 +1,15 @@
+    package main
+
+    import(
+        "net/http"
+        "log"
+        "fmt"
+    )
+    func main()  {
+        http.HandleFunc("/", Inicio)
+		log.Println("Servidor running...")
+		http.ListenAndServe(":8080", nil)
+    }
+    func Inicio (w http.ResponseWriter,r *http.Request ) {
+        fmt.Fprintf(w, "Hola Raul")
+    }
